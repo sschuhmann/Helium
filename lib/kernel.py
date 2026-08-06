c342e7e3618bbfc0dafce6d271e48dce1cc048db
deathaxe <16542113+deathaxe@users.noreply.github.com>
2026-08-06 11:06:59 +0200

    Minimize CI/dev dependencies (#163)
    This commit removes poetry and related dependency specifications to avoid
    unnecessary security advisories due to out-dated unmaintained tooling versions.

--- lib/kernel.py
+++ lib/kernel.py
@@ -4,6 +4,7 @@ KernelConnection class provides interaction with Jupyter kernels.
 
 Copyright (c) 2017-2018, NEGORO Tetsuya (https://github.com/ngr-t)
 """
+
 import re
 from collections import defaultdict
 from datetime import datetime
